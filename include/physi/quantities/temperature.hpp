390bb8734718ebce1279a9f7a36213d12ce7e7dc
Filippo Marcantonio <filippomarcantonio005@gmail.com>
2026-02-06 23:36:07 +0100

    feat: temperature conversions

--- include/physi/quantities/temperature.hpp
+++ include/physi/quantities/temperature.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "../core/quantity.hpp"
+#include <iostream>
 
 namespace physi {
 
 PHYSI_QUANTITY_BEGIN(temperature)
 
 PHYSI_UNIT(temperature, K, 1.0)
+PHYSI_UNIT_INCREASE(temperature, C, 1.0, 273.15)
+PHYSI_UNIT_INCREASE(temperature, F, 0.555556, 459.67)
 
 PHYSI_QUANTITY_END(temperature)
 
