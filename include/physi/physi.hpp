5a42ababa4f8e8fdb49dda2c2738f0a72386735b
Filippo Marcantonio <filippomarcantonio005@gmail.com>
2026-02-06 23:11:39 +0100

    fix: filename and includes

--- include/physi/physi.hpp
+++ include/physi/physi.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
+#include "quantities/amount_of_substance.hpp"
+#include "quantities/electric_current.hpp"
 #include "quantities/length.hpp"
+#include "quantities/luminosity.hpp"
 #include "quantities/mass.hpp"
+#include "quantities/temperature.hpp"
