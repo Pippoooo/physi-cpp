d3cde1b19b34018fbcf299ecc2a2c9b4b7ee3cdc
Filippo Marcantonio <filippomarcantonio005@gmail.com>
2026-02-07 14:00:30 +0100

    feat: example

--- examples/minimal_example.cpp
+++ examples/minimal_example.cpp
@@ -18,6 +18,20 @@ int main() {
     length_ld ll = l_ft2 + l_ft3; // mixed-precision arithmetic -> long double
     length_f ll2 = l_ft2 + ll;    // converts down to float alias
 
+    time_f t = 10_s;
+
+    speed_f s = 10_m / 10_s; // automatic unit conversion with operations.
+    s.km_h();
+
+    // inverse operations work too!
+    length_f travel = s * 10_s;
+
+    acceleration_f a = s / 10_s;
+    acceleration_f a2 = 10_m / 10_s / 10_s;
+    acceleration_f a3 = 10_km_h / 10_s;
+
+    speed_f accellerated = a3 * 2.5_s;
+
     std::cout << ll2.m() << " m, " << m1.lb() << " lb, " << ll.ft() << " ft\n";
 
     return 0;
